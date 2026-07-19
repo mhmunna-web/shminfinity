80071877184f62c9d474d2adfa6f2f1a3efbe63e
Mahmudul Hasan <mahmudulhasan@MacBook-Pro.local>
2026-07-20 01:28:52 +0300

    Add Microsoft Clarity

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga4";
+import clarity from "@microsoft/clarity";
 
 import "./index.css";
 import App from "./App.jsx";
 
-// Initialize Google Analytics 4
+// Google Analytics 4
 ReactGA.initialize("G-H0B9RR0W0V");
+
+// Microsoft Clarity
+clarity.init("xp3l526q8a");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
