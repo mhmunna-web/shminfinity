455f150317b6d872d4da49cc44469858c70cc383
Mahmudul Hasan <mahmudulhasan@MacBook-Pro.local>
2026-07-19 21:35:40 +0300

    Add Google Analytics 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 import "./index.css";
 import App from "./App.jsx";
+
+// Initialize Google Analytics 4
+ReactGA.initialize("G-H0B9RR0W0V");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
